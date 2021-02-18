52714a40d67bd5c386f7e40e01991b4e033df82d
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2021-02-18 18:24:25 -0300

    add items controller e points controller

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -7,6 +7,6 @@ export async function seed(Knex: Knex) {
         { title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
         { title: 'Resíduos Eletrônicos', image: 'eletronicos.svg' },
         { title: 'Resíduos Orgânicos', image: 'organicos.svg' },
-        { title: 'Óleo e Cozinha', image: 'oleo.svg' },
+        { title: 'Óleo de Cozinha', image: 'oleo.svg' },
     ]);
 }
